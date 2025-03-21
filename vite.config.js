c88589e16c312a6314aefb30df35c597f163748d
yosket <yosket87@gmail.com>
2025-03-21 12:00:00 +0900

    Implement feature in vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { getPlugin } from 'react-svg-module'
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    open: true,
+  },
+  static:{
+    directory:'public',
+    serveDirectory: true
+  },
+  plugins: [react(), getPlugin()],
+})
+
